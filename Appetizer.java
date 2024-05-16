133d1b699e3c99dd8dbdada76557530e4d21fa6c
smmurali <157665155+smmurali@users.noreply.github.com>
2024-05-16 19:54:45 -0400

    Create Appetizer.java

---
+++ Appetizer.java
@@ -0,0 +1,8 @@
+public class Appetizer extends MenuItem {
+    public Appetizer(String name, double price) {
+        super(name, price);
+    }
+    public String getCategory() {
+        return "Appetizer";
+    }
+}
