682da22a4d4af008de1514f7b5f6f0e1a6b51f75
smmurali <157665155+smmurali@users.noreply.github.com>
2024-05-16 19:56:21 -0400

    Create Dessert.java

---
+++ Dessert.java
@@ -0,0 +1,8 @@
+public class Dessert extends MenuItem {
+    public Dessert(String name, double price) {
+        super(name, price);
+    }
+    public String getCategory() {
+        return "Dessert";
+    }
+}
